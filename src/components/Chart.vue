8da7bbaa61a9ed12e56352e85d42cf9a4b9b6a63
Libin <1064048947@qq.com>
2020-08-19 22:16:43 +0800

    echarts

---
+++ src/components/Chart.vue
@@ -0,0 +1,59 @@
+<template>
+<!--  style="height: 400px" 组件挂载的 style 会自动挂载到组件的根节点上 -->
+  <div ref="chartDom"></div>
+</template>
+
+<script>
+import echarts from "echarts";
+import debounce from 'lodash/debounce'
+import { addListener, removeListener } from 'resize-detector'
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    option: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  watch: {
+    // 深度监听这么大的数据很耗性能，还是直接监听，只是在父组件中需要 this.option = {...this.option} 改变其内存地址
+    // option: {
+    //   handler(val) {
+    //     this.chart.setOption(val)
+    //   },
+    //   deep: true
+    // }
+    option(val) {
+      this.chart.setOption(val)
+    }
+  },
+  created() {
+    this.resize = debounce(this.resize, 200)
+  },
+  mounted() {
+    this.renderChart()
+    addListener(this.$refs.chartDom, this.resize)
+  },
+  beforeDestroy() {
+    removeListener(this.$refs.chartDom, this.resize)
+    // 销毁，置空， 防止内存泄漏
+    this.chart.dispose()
+    this.chart = null
+  },
+  methods: {
+    resize() {
+      console.log('resoze')
+      this.chart.resize()
+    },
+    renderChart() {
+      this.chart = echarts.init(this.$refs.chartDom);
+      this.chart.setOption(this.option);
+    }
+  }
+};
+</script>
+
+<style lang="less">
+</style>
--- src/views/dashboard/analysis.vue
+++ src/views/dashboard/analysis.vue
@@ -1,11 +1,77 @@
 <template>
   <div class="analysis">
-    hhh
+    <Chart :option='chartOption' style='height: 400px' />
   </div>
 </template>
 
 <script>
-export default {};
+import Chart from "@/components/Chart";
+const option = {
+  xAxis: {
+    type: "category",
+    boundaryGap: false
+  },
+  yAxis: {
+    type: "value",
+    boundaryGap: [0, "30%"]
+  },
+  visualMap: {
+    type: "piecewise",
+    show: false,
+    dimension: 0,
+    seriesIndex: 0,
+    pieces: [
+      {
+        gt: 1,
+        lt: 3,
+        color: "rgba(0, 180, 0, 0.5)"
+      },
+      {
+        gt: 5,
+        lt: 7,
+        color: "rgba(0, 180, 0, 0.5)"
+      }
+    ]
+  },
+  series: [
+    {
+      type: "line",
+      smooth: 0.6,
+      symbol: "none",
+      lineStyle: {
+        color: "green",
+        width: 5
+      },
+      markLine: {
+        symbol: ["none", "none"],
+        label: { show: false },
+        data: [{ xAxis: 1 }, { xAxis: 3 }, { xAxis: 5 }, { xAxis: 7 }]
+      },
+      areaStyle: {},
+      data: [
+        ["2019-10-10", 200],
+        ["2019-10-11", 400],
+        ["2019-10-12", 650],
+        ["2019-10-13", 500],
+        ["2019-10-14", 250],
+        ["2019-10-15", 300],
+        ["2019-10-16", 450],
+        ["2019-10-17", 300],
+        ["2019-10-18", 100]
+      ]
+    }
+  ]
+};
+export default {
+  data() {
+    return {
+      chartOption: option
+    }
+  },
+  components: {
+    Chart
+  }
+};
 </script>
 
 <style></style>
